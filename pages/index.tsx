373a573ee780dbdc9d0684818a4abb8de31b2c56
jlomas <jlomas@ceaaces.gob.ec>
2023-07-31 16:22:21 -0500

    feat: Navbar menu and mobile menu

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
-import useCurrentUser from '@/hooks/useCurrentUser';
+import Navbar from '@/components/Navbar';
 import {NextPageContext} from 'next';
-import { getSession, signOut } from 'next-auth/react';
+import { getSession } from 'next-auth/react';
 
 export async function getServerSideProps(context: NextPageContext){
 	const session = await getSession(context)
@@ -19,15 +19,11 @@ export async function getServerSideProps(context: NextPageContext){
 	}
 }
 
-export default function Home() {
-
-	const {data: user} = useCurrentUser()
+export default function Home() {	
 
 	return (
 		<>
-			<h1 className="text-2xl text-green-500">Netflix Clone</h1>
-			<p className='text-white'>Logged in as: {user?.name}</p>
-			<button className='h-10 w-full bg-white' onClick={() => signOut()}>Logout!</button>
+			<Navbar />
 		</>
 	);
 }
